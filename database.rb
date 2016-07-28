7f7a215513a9896569adfb16422337ea12d19e31
Bonnie Iker <bonniejiker@gmamil.com>
2016-07-28 09:31:34 -0400

    Almost finished, just need to add a delete function and fix spacing issues.

---
+++ database.rb
@@ -0,0 +1,74 @@
+class Person
+attr_accessor :name, :phone, :address, :position, :salary, :github
+
+  def initialize(name, phone, address, position, salary, github)
+  @name = name
+  @phone = phone
+  @address = address
+  @position = position
+  @salary = salary
+  @github = github
+end
+end
+
+
+class Menu
+  def initialize
+    @people = []
+  end
+
+  def prompt
+
+loop do
+   puts "A for Add a person"
+   puts "S for Search for a person"
+   puts "D for Delete a person"
+   puts "E for Exit"
+   choice = gets.chomp
+   break if choice == "E"
+
+ if choice == "A"
+   puts "Enter employee first and last name"
+     name = gets.chomp
+   puts "Enter employee phone number"
+     phone = gets.chomp
+   puts "Enter employee address"
+    address = gets.chomp
+   puts "Employee's position"
+    position = gets.chomp
+   puts "Employee's salary"
+    salary = gets.chomp
+   puts "Employee's github account"
+    github = gets.chomp
+    person = Person.new(name, phone, address, position, salary, github)
+    @people << person
+    p @people
+ elsif choice == "S"
+   puts "Search by name"
+    input_name = gets.chomp
+    for person in @people
+      if person.name == input_name
+
+         puts "Employee is listed:
+              #{person.name}
+              #{person.phone}
+              #{person.address}
+              #{person.position}
+              #{person.salary}
+              #{person.github}"
+
+  else puts "Person not found"
+  if choice == "D"
+  puts "Delete which Employee? "
+    gets.chomp
+
+end
+end
+end
+end
+end
+end
+end
+menu = Menu.new
+menu.prompt
+p @people
